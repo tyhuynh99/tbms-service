ba0b6f789de726894c28971961834df57b3bdaa9
Tran Phuc <ttran.phuc99@gmail.com>
2022-07-27 17:29:12 +0700

    init source

---
+++ src/main/java/com/shop/tbms/TbmsApplication.java
@@ -0,0 +1,13 @@
+package com.shop.tbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/tbms/TbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.tbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
