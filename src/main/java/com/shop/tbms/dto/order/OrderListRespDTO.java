4dc6d95d471c384978a82f980cb04c75a2331e21
Tran Phuc <ttran.phuc99@gmail.com>
2022-09-05 23:40:46 +0700

    response formatted date

--- src/main/java/com/shop/tbms/dto/order/OrderListRespDTO.java
+++ src/main/java/com/shop/tbms/dto/order/OrderListRespDTO.java
@@ -1,5 +1,6 @@
 package com.shop.tbms.dto.order;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.shop.tbms.enumerate.OrderStatus;
 import lombok.*;
 
@@ -13,9 +14,10 @@ import java.time.LocalDate;
 public class OrderListRespDTO {
     private Long id;
     private String code;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate deliveredDate;
-    private Boolean isUrgent;
-    private Boolean isLate;
+    private boolean isUrgent;
+    private boolean isLate;
     private OrderStatus status;
     private Long numOfMold;
     private String procedureName;
