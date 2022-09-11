c59fafb1c818ac108a47f19fc04a337e4557e5dc
Huynh Phu Ty <tyhuynh1199@gmail.com>
2022-09-11 21:24:43 +0700

    update starting service file

--- for_vps/start-tbms.sh
+++ for_vps/start-tbms.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Begin starting TBMS Service"
-nohup java -jar ./source/github/workspace/target/tbms-build.jar > /dev/null 2>&1&
+
+DB_SERVER="127.0.0.1"
+DB_PORT="5432"
+DB_USERNAME="admin"
+DB_PASSWORD="123"
+ADMIN_KEY_FILE_PATH="/home/admin0099/tbms-a082d-firebase-adminsdk-43tdy-6fc21f54ab.json"
+BUCKET_NAME="tbms-a082d.appspot.com"
+PREVIEW_URL="https://firebasestorage.googleapis.com/v0/b/tbms-a082d.appspot.com/o/%s?alt=media"
+
+nohup java -jar ./source/github/workspace/target/tbms-build.jar -Dspring.profiles=dev -DDB_SERVER=$DB_SERVER -DDB_PORT=$DB_PORT -DDB_USERNAME=$DB_USERNAME -DDB_PASSWORD=$DB_PASSWOR>
 echo "End starting TBMS Service"
