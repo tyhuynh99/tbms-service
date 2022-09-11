61def9b9d9e69ed110a397977758dd8a2de4a6f8
Tran Phuc <ttran.phuc99@gmail.com>
2022-09-11 17:05:51 +0700

    add template csv data

---
+++ sql/delete order data.sql
@@ -0,0 +1,15 @@
+/* truncate data */
+truncate table 
+	step_sequence, 
+	step, 
+	checklist, 
+	evidence, 
+	issue, 
+	mold_progress, 
+	mold_element,
+	report_log, 
+	issue_mold_detail, 
+	mold,
+	monitor_scheduler,
+	purchase_order,
+	"procedure";
