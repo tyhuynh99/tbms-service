1c4a27a3f63365ecc647fc8accad3b70fc865045
Huynh Phu Ty <tyhuynh1199@gmail.com>
2022-09-17 15:25:31 +0700

    compress image

--- src/main/java/com/shop/tbms/constant/CommonConstant.java
+++ src/main/java/com/shop/tbms/constant/CommonConstant.java
@@ -1,5 +1,9 @@
 package com.shop.tbms.constant;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CommonConstant {
     public static final String RES_JSON_TYPE = "application/json";
+    public static final List<String> IMAGE_CONTENT_TYPE = Arrays.asList("image/png", "image/jpg", "image/jpeg");
 }
--- src/main/java/com/shop/tbms/service/impl/FileServiceImpl.java
+++ src/main/java/com/shop/tbms/service/impl/FileServiceImpl.java
@@ -11,12 +11,21 @@ import com.shop.tbms.dto.FileDTO;
 import com.shop.tbms.service.FileService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import static com.shop.tbms.constant.CommonConstant.IMAGE_CONTENT_TYPE;
 
 @Slf4j
 @Service
@@ -60,19 +69,19 @@ public class FileServiceImpl implements FileService {
     @Override
     public FileDTO upload(MultipartFile multipartFile) throws Exception {
         log.info("Start upload file {}", multipartFile.getOriginalFilename());
-        // TODO: Step 0 validate
+        // Step 0 validate TODO: validate for video
         validate(multipartFile);
-        // TODO: Step 1 resize and compress file
-
+        // Step 1 resize and compress file TODO: compress for video
+        MultipartFile compressedFile = compressImage(multipartFile);
         // TODO: Step 2 generate file name
-        String filename = multipartFile.getOriginalFilename();
+        String filename = generateFilename(compressedFile);
 
         // Step 3 upload to firebase
-        Blob result = bucket.create(filename, multipartFile.getBytes(), multipartFile.getContentType());
+        Blob result = bucket.create(filename, compressedFile.getBytes(), compressedFile.getContentType());
         result.createAcl(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
         String viewUrl = String.format(previewUrl, filename);
 
-        log.info("Upload file {} success with url {}", multipartFile.getOriginalFilename(), viewUrl);
+        log.info("Upload file {} success with url {}", compressedFile.getOriginalFilename(), viewUrl);
         return FileDTO.builder()
                 .filename(filename)
                 .url(viewUrl)
@@ -90,8 +99,56 @@ public class FileServiceImpl implements FileService {
     }
 
     private boolean isSupportedContentType(String contentType) {
-        return contentType.equals("image/png")
-                || contentType.equals("image/jpg")
-                || contentType.equals("image/jpeg");
+        return IMAGE_CONTENT_TYPE.contains(contentType);
+    }
+
+    private MultipartFile compressImage(MultipartFile file) {
+        float quality = 0.3f; // Best practice value for compression
+
+        String name = file.getName();
+        String imageName = file.getOriginalFilename();
+        String contentType = file.getContentType();
+        String imageExtension = imageName.substring(imageName.lastIndexOf(".") + 1);
+
+        // Returns an Iterator containing all currently registered ImageWriters that claim to be able to encode the named format.
+        // You don't have to register one yourself; some are provided.
+        ImageWriter imageWriter = ImageIO.getImageWritersByFormatName(imageExtension).next();
+        ImageWriteParam imageWriteParam = imageWriter.getDefaultWriteParam();
+        imageWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT); // Check the api value that suites your needs.
+
+        // A compression quality setting of 0.0 is most generically interpreted as "high compression is important,"
+        // while a setting of 1.0 is most generically interpreted as "high image quality is important."
+        imageWriteParam.setCompressionQuality(quality);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        // MemoryCacheImageOutputStream: An implementation of ImageOutputStream that writes its output to a regular
+        // OutputStream, i.e. the ByteArrayOutputStream.
+        ImageOutputStream imageOutputStream = new MemoryCacheImageOutputStream(baos);
+
+        // Sets the destination to the given ImageOutputStream or other Object.
+        imageWriter.setOutput(imageOutputStream);
+        BufferedImage originalImage = null;
+        try (InputStream inputStream = file.getInputStream()) {
+            originalImage = ImageIO.read(inputStream);
+        } catch (IOException e) {
+            String info = String.format("compressImage - bufferedImage (file %s)- IOException - message: %s ", imageName, e.getMessage());
+            log.error(info);
+            return new MockMultipartFile(name, imageName, contentType, baos.toByteArray());
+        }
+        IIOImage image = new IIOImage(originalImage, null, null);
+        try {
+            imageWriter.write(null, image, imageWriteParam);
+        } catch (IOException e) {
+            String info = String.format("compressImage - imageWriter (file %s)- IOException - message: %s ", imageName, e.getMessage());
+            log.error(info);
+        } finally {
+            imageWriter.dispose();
+        }
+
+        return new MockMultipartFile(name, imageName, contentType, baos.toByteArray());
+    }
+
+    private String generateFilename(MultipartFile file) {
+        return file.getOriginalFilename();
     }
 }
