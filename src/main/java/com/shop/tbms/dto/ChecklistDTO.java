7d4ec14b8e15f7bed34891c157641f2d7443403b
Tran Phuc <ttran.phuc99@gmail.com>
2022-10-16 20:03:37 +0700

    change field to checklistId in step detail

--- src/main/java/com/shop/tbms/dto/ChecklistDTO.java
+++ src/main/java/com/shop/tbms/dto/ChecklistDTO.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChecklistDTO {
-    private Long id;
+    private Long checklistId;
     private String content;
     private Boolean isChecked;
 }
--- src/main/java/com/shop/tbms/mapper/ChecklistMapper.java
+++ src/main/java/com/shop/tbms/mapper/ChecklistMapper.java
@@ -3,11 +3,13 @@ package com.shop.tbms.mapper;
 import com.shop.tbms.dto.ChecklistDTO;
 import com.shop.tbms.entity.Checklist;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ChecklistMapper {
+    @Mapping(target = "checklistId", source = "id")
     ChecklistDTO toDTO(Checklist checklist);
 
     List<ChecklistDTO> toDTOs(List<Checklist> checklists);
