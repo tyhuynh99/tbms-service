aa870e607e77e832df9885e7a38f1378121e824f
Tran Phuc <ttran.phuc99@gmail.com>
2022-11-15 08:05:28 +0700

    add profiledto

---
+++ src/main/java/com/shop/tbms/dto/ProfileDTO.java
@@ -0,0 +1,24 @@
+package com.shop.tbms.dto;
+
+import com.shop.tbms.config.security.TbmsUserDetails;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProfileDTO extends TbmsUserDetails {
+    public ProfileDTO(TbmsUserDetails userDetails) {
+        super(
+                userDetails.getUserId(),
+                userDetails.getUsername(),
+                userDetails.getFullname(),
+                userDetails.getRole(),
+                userDetails.getActive(),
+                userDetails.getPosition(),
+                userDetails.getUpdatedDate());
+    }
+
+    private boolean hasUnreadNoti;
+}
