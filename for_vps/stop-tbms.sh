90e0d85dfb8489f97afa0454a64b2fc6baf2b388
Huynh Phu Ty <tyhuynh1199@gmail.com>
2023-08-14 21:37:06 +0700

    update config

--- for_vps/stop-tbms.sh
+++ for_vps/stop-tbms.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Begin stopping TBMS Service"
 sudo pkill -f ./source/github/workspace/target/tbms-build.jar
+sudo pkill -f ~/service/tbms-build.jar
 sudo rm ./source/github/workspace/target/tbms-build.jar
 echo "End stopping TBMS Service"
